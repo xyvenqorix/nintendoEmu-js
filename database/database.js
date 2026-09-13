81a9c5bce0be308bd38dfd4597882f7cfbb5c8ed
rodol_ugc <rodolfo2ceo@gmail.com>
2026-09-13 15:53:20 -0400

    Update database.js

--- database/database.js
+++ database/database.js
@@ -31,7 +31,7 @@ const juegos = [
     id: "Harvest Moon_gba",
         name: "Harvest Moon - Friends of Mineral Town",
         rom: "includes/room/Harvest Moon - Friends of Mineral Town (Europe).gba",
-        cover: "Harvestmoon_box_snes.webp",
+        cover: "Harvest+Moon+(USA)-image.jpg",
         system: "gba"
   },
     
